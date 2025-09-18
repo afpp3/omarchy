29128c265945d95e06d2242f602b408558e4ea37
Andre Felippe <afelipp3@gmail.com>
2025-09-17 23:03:22 -0300

    Move Fish config files to config directory
    The commit moves Fish shell configuration files from default/ to config/
    and adds new fish-related files like completions and starship themes.
    
    Files moved:
    - Fish config files from default/ to config/
    - Added bun completions
    - Added starship prompt themes
    - Added fish style changer script
    
    Added:
    - change-style.sh for switching starship themes
    - bun.fish completions
    - Multiple starship theme variations

--- install/config/config.sh
+++ install/config/config.sh
@@ -4,3 +4,6 @@ cp -R ~/.local/share/omarchy/config/* ~/.config/
 
 # Use default bashrc from Omarchy
 cp ~/.local/share/omarchy/default/bashrc ~/.bashrc
+
+# Use default fish from Omarchy Custom
+cp ~/.local/share/omarchy/config/fish ~/.config/fish
